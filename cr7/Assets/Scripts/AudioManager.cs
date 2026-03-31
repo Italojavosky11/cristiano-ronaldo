e0f70e27dc1eaff861464aa34b2dbe937542c32b
Italo Javosky <silva.camilo@escolar.ifrn.edu.br>
2026-03-31 10:21:21 -0300

    sfs
    sf

--- cr7/Assets/Scripts/AudioManager.cs
+++ cr7/Assets/Scripts/AudioManager.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
 
 public class AudioManager : MonoBehaviour
 {
-    #region Singleton
-
+    private AudioSource systemSource;
+    private List<AudioSource> activeSources;
     
-
+    #region Singleton
     
     public static AudioManager Instance;
     
@@ -16,6 +20,8 @@ public class AudioManager : MonoBehaviour
        {
            Instance = this;
            DontDestroyOnLoad(gameObject);
+           systemSource = GetComponent<AudioSource>();
+           activeSources = new List<AudioSource>();
        }
        else
        {
@@ -27,4 +33,41 @@ public class AudioManager : MonoBehaviour
    
    #endregion
 
+   #region AudioControls
+
+   public void Play(AudioClip clip, AudioSource source)
+   {
+       if(!activeSources.Contains(source))
+           activeSources.Add(source);
+       source.Stop();
+       source.clip = clip;
+       source.Play();
+   }
+
+   public void Stop(AudioSource source)
+   {
+       if (activeSources.Contains(source))
+            source.Stop(); 
+       activeSources.Remove(source);
+       
+   }
+   public void Pause(AudioSource source)
+   {
+       if (activeSources.Contains(source))
+            source.Pause();
+   }
+
+   public void Resume(AudioSource source)
+   {
+       if (activeSources.Contains(source))
+            source.UnPause();
+   }
+   
+
+   public void PlayerOneShot(AudioClip clip,AudioSource source)
+   {
+       source.PlayOneShot(clip);
+   }
+   
+   #endregion
 }
